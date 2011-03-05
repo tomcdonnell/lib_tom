902631e3b07ef7c8a5ab3286636904f98c89e6ad
tom <tom@1bca179a-8c20-481a-88b6-da998f530761>
2011-03-05 12:59:15 +0000

    Split vector.js into two files.
    git-svn-id: http://tomcdonnell.net/svn/projects@309 1bca179a-8c20-481a-88b6-da998f530761

---
+++ tom/js/physics/VectorPol2d.js
@@ -0,0 +1,84 @@
+/**************************************************************************************************\
+*
+* vim: ts=3 sw=3 et wrap co=100 go-=b
+*
+* Filename: "vector.js"
+*
+* Project: Library.
+*
+* Purpose: Mathematical vector objects and functions.
+*          NOTE: For a more complete vector library, see C++ file "home/tom/TomsLibrary/vector.h".
+*
+* Author: Tom McDonnell 08/11/2007
+*
+\**************************************************************************************************/
+
+// Object definition. //////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Two dimensional polar vector.
+ */
+function VectorPol2d(r, angle)
+{
+   var f = 'VectorPol2d()';
+   UTILS.checkArgs(f, arguments, [Number, Number]);
+   UTILS.assert(f, 0, 0 <= r);
+   UTILS.assert(f, 1, -Math.PI <= angle && angle <= Math.PI);
+
+   // Getters. --------------------------------------------------------------------------------//
+
+   this.getR     = function () {return     r;};
+   this.getAngle = function () {return angle;};
+
+   this.getComponentY = function () {return r * Math.sin(angle);};
+   this.getComponentX = function () {return r * Math.cos(angle);};
+
+   // Setters. --------------------------------------------------------------------------------//
+
+   this.setR = function (r1)
+   {
+      var f = 'VectorPol2d()';
+      UTILS.checkArgs(f, arguments, [Number]);
+      UTILS.assert(f, 0, r1 >= 0);
+
+      r = r1;
+   };
+
+   this.setAngle = function (a)
+   {
+      var f = 'VectorPol2d()';
+      UTILS.checkArgs(f, arguments, [Number]);
+      UTILS.assert(f, 0, -Math.PI <= a && a <= Math.PI);
+
+      angle = a;
+   };
+
+   // Operations. -----------------------------------------------------------------------------//
+
+   this.multiply = function (a)
+   {
+      var f = 'VectorPol2d.multiply()';
+      UTILS.checkArgs(f, arguments, [Number]);
+
+      return new VectorPol2d
+      (
+         r * Math.abs(a), ((a < 0)? (angle + ((angle < 0)? Math.PI: -Math.PI)): angle)
+      );
+   };
+
+   this.rotate = function (a) {angle += a; if (a > Math.PI) angle -= twoPi;};
+
+   this.convToRec = function ()
+   {
+      return new VectorRec2d(this.getComponentX(), this.getComponentY());
+   };
+
+   this.clone = function () {return new VectorPol2d(r, angle);};
+
+   this.asString = function ()
+   {
+      return string = 'VectorPol2d(r: ' + r + '; angle: ' + angle + ')';
+   };
+};
+
+/*******************************************END*OF*FILE********************************************/
---
+++ tom/js/physics/VectorRec2d.js
@@ -0,0 +1,95 @@
+/**************************************************************************************************\
+*
+* vim: ts=3 sw=3 et wrap co=100 go-=b
+*
+* Filename: "vector.js"
+*
+* Project: Library.
+*
+* Purpose: Mathematical vector objects and functions.
+*          NOTE: For a more complete vector library, see C++ file "home/tom/TomsLibrary/vector.h".
+*
+* Author: Tom McDonnell 08/11/2007
+*
+\**************************************************************************************************/
+
+// Object definition. //////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Two dimensional rectangular vector.
+ */
+function VectorRec2d(x, y)
+{
+   var f = 'VectorRec2d()';
+   UTILS.checkArgs(f, arguments, [Number, Number]);
+
+   // Getters. --------------------------------------------------------------------------------//
+
+   this.getX         = function () {return x;};
+   this.getY         = function () {return y;};
+   this.getMagnitude = function () {return Math.sqrt(x * x + y * y);};
+   this.getAngle     = function () {return Math.atan2(y, x);        };
+
+   // Setters. --------------------------------------------------------------------------------//
+
+   this.setX = function (x1)
+   {
+      var f = 'VectorRec2d.setX()';
+      UTILS.checkArgs(f, arguments, [Number]);
+
+      x = x1;
+   };
+
+   this.setY = function (y1)
+   {
+      var f = 'VectorRec2d.setY()';
+      UTILS.checkArgs(f, arguments, [Number]);
+
+      y = y1;
+   };
+
+   // Operations. -----------------------------------------------------------------------------//
+
+   this.add = function (a)
+   {
+      var f = 'VectorRec2d.add()';
+      UTILS.checkArgs(f, arguments, [VectorRec2d]);
+
+      return new VectorRec2d(x + a.getX(), y + a.getY());
+   };
+
+   this.subtract = function (a)
+   {
+      var f = 'VectorRec2d.subtract()';
+      UTILS.checkArgs(f, arguments, [VectorRec2d]);
+
+      return new VectorRec2d(x - a.getX(), y - a.getY());
+   };
+
+   this.multiply = function (a)
+   {
+      var f = 'VectorRec2d.multiply()';
+      UTILS.checkArgs(f, arguments, [Number]);
+
+      return new VectorRec2d(x * a, y * a);
+   };
+
+   this.divide = function (a)
+   {
+      var f = 'VectorRec2d.divide()';
+      UTILS.checkArgs(f, arguments, [Number]);
+
+      return new VectorRec2d(x / a, y / a);
+   };
+
+   this.convToPol = function () {return new VectorPol2d(this.getMagnitude(), this.getAngle());};
+
+   this.clone = function () {return new VectorRec2d(x, y);};
+
+   this.asString = function ()
+   {
+      return string = 'VectorRec2d(x: ' + x + '; y: ' + y + ')';
+   };
+}
+
+/*******************************************END*OF*FILE********************************************/
